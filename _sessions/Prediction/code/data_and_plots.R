32c7c64c7fa8f1e2a78fd26fda1e66dd2deff136
Dirk Wulff <dirkwulff@fp-ws-b210.psycho.unibas.ch>
2020-04-02 18:09:10 +0200

    ...

--- _sessions/Prediction/code/data_and_plots.R
+++ _sessions/Prediction/code/data_and_plots.R
@@ -59,4 +59,63 @@ graduation_test = college_test %>%
 write_csv(graduation_train, '_sessions/Prediction/1_Data/graduation_train.csv')
 write_csv(graduation_test, '_sessions/Prediction/1_Data/graduation_test.csv')
 
+cat(paste0('|',names(graduation_train),'|  |\n'),sep='')
+
+
+
+house_train = read_csv('_sessions/Prediction/1_Data/house_train_eng.csv')
+house_test = read_csv('_sessions/Prediction/1_Data/house_test_eng.csv')
+
+
+cat(paste0('  = ',names(house_train),',\n'),sep='')
+
+house_train = house_train = rename(
+  Preis  = price,
+  Schlafzimmer  = bedrooms,
+  Baeder  = bathrooms,
+  Gesamt_sqft  = sqft_living,
+  Grundstück_sqft  = sqft_lot,
+  Stockwerke  = floors,
+  Uferlage  = waterfront,
+  Besichtigung  = view,
+  Zustand  = condition,
+  Einstufung  = grade,
+  Wohnraum_sqft  = sqft_above,
+  Keller_sqft  = sqft_basement,
+  Baujahr  = yr_built,
+  Renovationsjahr  = yr_renovated,
+  Postleitzahl  = zipcode,
+  Breitengrad  = lat,
+  Längengrad  = long,
+  Gesamt_sqft_2015  = sqft_living15,
+  Grundstück_sqft_2015  = sqft_lot15
+  )
+
+
+house_test = house_test = rename(
+  Preis  = price,
+  Schlafzimmer  = bedrooms,
+  Baeder  = bathrooms,
+  Gesamt_sqft  = sqft_living,
+  Grundstück_sqft  = sqft_lot,
+  Stockwerke  = floors,
+  Uferlage  = waterfront,
+  Besichtigung  = view,
+  Zustand  = condition,
+  Einstufung  = grade,
+  Wohnraum_sqft  = sqft_above,
+  Keller_sqft  = sqft_basement,
+  Baujahr  = yr_built,
+  Renovationsjahr  = yr_renovated,
+  Postleitzahl  = zipcode,
+  Breitengrad  = lat,
+  Längengrad  = long,
+  Gesamt_sqft_2015  = sqft_living15,
+  Grundstück_sqft_2015  = sqft_lot15
+)
+
+write_csv(house_train, '_sessions/Prediction/1_Data/house_train.csv')
+write_csv(house_test, '_sessions/Prediction/1_Data/house_test.csv')
+
+
 
